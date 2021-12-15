da77954e74c4fd2556031eb7936d9c6035f66eba
yuzequn095 <yuzequn095@gmail.com>
2021-12-15 10:57:08 -0800

    user verification, login promoption

---
+++ src/main/java/com/spring/admin/bean/User.java
@@ -0,0 +1,13 @@
+package com.spring.admin.bean;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String username;
+    private String password;
+
+}
+
--- src/main/java/com/spring/admin/controller/PageController.java
+++ src/main/java/com/spring/admin/controller/PageController.java
@@ -4,12 +4,21 @@
  */
 package com.spring.admin.controller;
 
+import com.spring.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class PageController {
+
+    private String test_username = "admin";
+    private String test_password = "admin";
 
     /*
      * Accept request
@@ -25,9 +34,27 @@ public class PageController {
      * with the post message from login
      */
     @PostMapping("/login")
-    public String mainHandler(String username, String password){
-        // return "main";
-        return "redirect:/main.html"; // no longer post /login, redirect to main page
+    public String mainHandler(User user, HttpSession session, Model model){
+
+        // check if username and password is empty
+        //if(StringUtils.hasLength(user.getUsername()) && StringUtils.hasLength(user.getPassword())){
+        if(test_username.equals(user.getUsername()) && test_password.equals(user.getPassword())){
+            // store the user information
+            session.setAttribute("login_user", user);
+
+            // return "main";
+            return "redirect:/main.html"; // no longer post /login, redirect to main page
+            // return "main";
+        }
+
+        // else back to login page
+        else{
+
+            model.addAttribute("msg", "incorrect username or password");
+
+            return "login";
+        }
+
     }
 
     /*
@@ -35,8 +62,19 @@ public class PageController {
      * Repeat post request to main
      */
     @GetMapping("/main.html")
-    public String mainPage(){
-        return "main";
+    public String mainPage(HttpSession session, Model model){
+
+        // check if user is in login status
+        // filter
+        Object loginUser = session.getAttribute("login_user");
+        if(loginUser != null){
+            return "main";
+        }
+        // else back to login
+        else{
+            model.addAttribute("msg", "no user information detected");
+            return "login";
+        }
     }
 
 }
